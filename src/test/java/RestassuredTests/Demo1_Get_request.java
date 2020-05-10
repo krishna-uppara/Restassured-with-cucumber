2fa53ebaade26a1e417bf58a1b2f4685ca9fb986
user.email <krishnaputtu1987@gmail.com>
2020-05-11 02:14:44 +0530

    jjj

--- src/test/java/RestassuredTests/Demo1_Get_request.java
+++ src/test/java/RestassuredTests/Demo1_Get_request.java
@@ -1,50 +1,72 @@
 package RestassuredTests;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Demo1_Get_request {
 	
-	@Test
-	public void getweatherdetails()
+	
+	
+	
+	@Test	
+	public void postdata()
 	{
+		RestAssured.baseURI="	http://dummy.restapiexample.com";
+		RestAssured.basePath="/api/v1/employees";
 		given()
 		.when()
-		     .get("http://restapi.demoqa.com/utilities/weather/city/bangalore")
+		     .get()
 		.then()
 		.statusCode(200)
-		.statusLine("HTTP/1.1 200 OK")
-		.body("City",equalTo("Bengaluru"))
-		.and()
-		.body("WindDirectionDegree",equalTo("220 Degree"))
-		.header("content-type","application/json")
-		.and()
-		.header("content-length",hasSize(166));
-		
-		
+		.statusLine("HTTP/1.1 200 OK");
 
 	}
 	
-	@Test
-	public void getreqresdetails()
-	{
-		given()
-		.when()
-		     .get("https://reqres.in/api/users?page=2")
-		.then()
-		.statusCode(200)
-		.statusLine("HTTP/1.1 200 OK")
-		.body("data.id",equalTo("7"))
-		.and()
-		.body("last_name",equalTo("Funke"))
-		.header("content-type","application/json")
-		.and()
-		.header("content-length",hasSize(166));
-		
-		
-
-	}
 	
+//	@Test
+//	public void getweatherdetails()
+//	{
+//		given()
+//		.when()
+//		     .get("http://restapi.demoqa.com/utilities/weather/city/bangalore")
+//		.then()
+//		.statusCode(200)
+//		.statusLine("HTTP/1.1 200 OK")
+//		.body("City",equalTo("Bengaluru"))
+//		.and()
+//		.body("WindDirectionDegree",equalTo("220 Degree"))
+//		.header("content-type","application/json")
+//		.and()
+//		.header("content-length",hasSize(166));
+//		
+//		
+//
+//	}
+//	
+//	@Test
+//	public void getreqresdetails()
+//	{
+//		given()
+//		.when()
+//		     .get("https://reqres.in/api/users?page=2")
+//		.then()
+//		.statusCode(200)
+//		.statusLine("HTTP/1.1 200 OK")
+//		.body("data.id",equalTo("7"))
+//		.and()
+//		.body("last_name",equalTo("Funke"))
+//		.header("content-type","application/json")
+//		.and()
+//		.header("content-length",hasSize(166));
+//		
+//		
+//
+//	}
+//	
 
 }
